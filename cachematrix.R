a7831a1dc8b6cc091ab7660fe15fe6bbafb2eb35
Soni1989 <soni4.k@tcs.com>
2017-04-21 16:07:00 +0200

    Adding description of the function
    Adding description of the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -18,7 +17,6 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
